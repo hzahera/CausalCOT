1e69d9111face8804ef7d856ec69df95c5ec33b1
hzahera <hamada.zahera@uni-paderborn.de>
2023-09-08 01:05:15 +0200

    save experiments

---
+++ src/Falcon_Student.py
@@ -0,0 +1,11 @@
+class Falcon_Student:
+
+    def __init__(self,):
+
+    def answer_questions(self, questions):
+
+
+    def load_dataset(self, dataset_path):
+
+
+    def save_answers(self, file_path):
